cbde924d0d2f7a4e64fce599bebdc8540e5784e4
LKLABSolution <124665088+LKLABSolution@users.noreply.github.com>
2023-02-10 17:50:30 +0900

    update

--- Ki4C_Solution/Controllers/Auth/SendMailController.cs
+++ Ki4C_Solution/Controllers/Auth/SendMailController.cs
@@ -8,8 +8,8 @@ namespace Ki4C_Solution.Controllers.Auth
         {
             return View();
         }
-        
-        
+
+
 
         //naver SMTP Send Method
         public string NaverSendMail()
@@ -18,32 +18,28 @@ namespace Ki4C_Solution.Controllers.Auth
             {
                 System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
                 //발신자
-                mail.From = new System.Net.Mail.MailAddress("임윤식" + "<" + "lsun000@naver.com" + ">");
+                mail.From = new System.Net.Mail.MailAddress("발신자명" + "<" + "발신자이메일주소" + ">");
 
                 //수신자
-                mail.To.Add("amsomad@lklab.org");
-                
+                mail.To.Add("수신자이메일주소");
+
                 //제목
                 mail.Subject = "발신Subject";
 
                 //내용
                 string bodyTag = @"
-                    <link href=""https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"" rel=""stylesheet"" integrity=""sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"" crossorigin=""anonymous"">
-                    <div class=""d-flex align-items-center vh-100"">
-                        <div class=""container text-center"">
-                            <div class=""row"">
-                                <div class=""col-lg-8 mx-auto"">
-                                    <h1 class=""display-4"">
-                                        <img src=""~/images/logo.png"" alt=""logo"" width=""80%"" height=""80%"" />
-                                    </h1>
-                                    <p class=""lead text-muted"">이메일 주소 출력란</p>
-                                    <p class=""lead text-muted"">정상 로그아웃 하였습니다.</p>
-                                    <p class=""lead text-muted"">수고하셨습니다.</p>
-                                    <a class=""btn btn-primary"" asp-area="" asp-controller=""Home"" asp-action=""index"">메인으로 이동</a>
-                                </div>
-                            </div>
-                        </div>
+                  <head>
+                    <link rel=""stylesheet"" href=""https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"" integrity=""sha384-UjKWGO0VxUiZxdyYjrvZgWkTIFhcTTwzHdVjK9sCe3l3xHfzqvFUKMtS8dZqzLjM"" crossorigin=""anonymous"">
+                  </head>
+                  <body>
+                    <div class=""container"">
+                      <p class=""h4"">Dear User,</p>
+                      <p>Your temporary password is: <strong>TEMPORARY_PASSWORD</strong></p>
+                      <p>Please use this password to log in and reset your password to a permanent one.</p>
+                      <p>Best regards,</p>
+                      <p>The Support Team</p>
                     </div>
+                  </body>
                  ";
                 mail.Body = bodyTag;
                 mail.IsBodyHtml = true;
@@ -53,12 +49,14 @@ namespace Ki4C_Solution.Controllers.Auth
                 smtp.Host = "smtp.naver.com";
                 smtp.Port = 587;
                 smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new System.Net.NetworkCredential("lsun000@naver.com", "dbsskfo!2");
+                //네이버 메일 계정
+                smtp.Credentials = new System.Net.NetworkCredential("발신자이메일주소", "패스워드");
                 smtp.EnableSsl = true;
                 smtp.Send(mail);
                 return "메일 전송 완료";
 
-            } catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 return "메일 전송 실패";
             }
